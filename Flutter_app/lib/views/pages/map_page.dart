57dedaf3b4b8b07e3e8c1dda890b714bfe3ca6bd
ZidYu <zecyu@mymail.mapua.edu.ph>
2025-10-27 08:14:04 +0800

    Fix for android map

--- Flutter_app/lib/views/pages/map_page.dart
+++ Flutter_app/lib/views/pages/map_page.dart
@@ -12,7 +12,7 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
-  late GoogleMapController mapController;
+  GoogleMapController? mapController;
   final LatLng _center = const LatLng(14.5995, 120.9842);
   Position? _currentPosition;
   bool _isLoadingLocation = false;
@@ -45,7 +45,7 @@ class _MapPageState extends State<MapPage> {
         });
 
         // Move camera to current location
-        mapController.animateCamera(
+  mapController?.animateCamera(
           CameraUpdate.newLatLngZoom(
             LatLng(position.latitude, position.longitude),
             15.0,
@@ -145,6 +145,15 @@ class _MapPageState extends State<MapPage> {
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+  }
+
+  @override
+  void dispose() {
+    // Dispose the map controller to free native resources (prevents
+    // leaking ImageReader/Surface buffers on Android when the page is
+    // opened/closed repeatedly).
+    mapController?.dispose();
+    super.dispose();
   }
 
   @override
@@ -267,7 +276,7 @@ class _MapPageState extends State<MapPage> {
                               subtitle: Text(store['vicinity']),
                               trailing: Text('${store['rating']} ⭐'),
                               onTap: () {
-                                mapController.animateCamera(
+                                mapController?.animateCamera(
                                   CameraUpdate.newLatLngZoom(
                                     LatLng(store['lat'], store['lng']),
                                     18.0,
